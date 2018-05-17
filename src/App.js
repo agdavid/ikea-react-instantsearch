6ea797faa3fad8e90ff9063fe14b20293ed75721
Ajay David <antonio.g.david@gmail.com>
2018-05-17 17:32:11 -0400

    add InstantSearch component and styling cdn

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import { InstantSearch } from 'react-instantsearch/dom';
 
 class App extends Component {
   render() {
@@ -8,6 +9,13 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">Welcome to IKEA with React-InstantSearch</h1>
         </header>
+        <InstantSearch
+          appId="ZSL0RJTCOE"
+          apiKey="3f46604e5ceef1479ae2ca58ae4f0a71"
+          indexName="ikea"
+        >
+
+        </InstantSearch>
       </div>
     );
   }
