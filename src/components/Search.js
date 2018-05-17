a72c0da202e2fbf3586f0a8d44c9e243404165d6
Ajay David <antonio.g.david@gmail.com>
2018-05-17 17:54:27 -0400

    add interactive SearchBox widget

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Hits } from 'react-instantsearch/dom';
+import { Hits, SearchBox } from 'react-instantsearch/dom';
 import Product from './Product';
 
 const Search = () => {
   return (
     <div className="container">
+      <SearchBox />
       <Hits hitComponent={Product}/>
     </div>
   );
