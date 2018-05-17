22d373a66adbec27d59e28bbedb66833cd5928c1
Ajay David <antonio.g.david@gmail.com>
2018-05-17 17:58:35 -0400

    add Highlight widget to search result

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Highlight } from 'react-instantsearch/dom';
 
 const Product = ({ hit }) => {
+
+  const productStyle = {
+    marginTop: '10px'
+  }
+
   return (
-    <div>
-      {hit.name} | {hit.price}
+    <div style={productStyle}>
+      <span className="hit-name">
+        <Highlight attribute="name" hit={hit} />
+        | {hit.price}
+      </span>
     </div>
   );
 }
